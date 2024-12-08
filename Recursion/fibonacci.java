263de0fc44d93a9b4bf37d830a3bc0b7b462ca67
Alfiya Heba <alfiyaheba10@gmail.com>
2024-12-08 20:45:23 +0530

    fibonacci

---
+++ Recursion/fibonacci.java
@@ -0,0 +1,15 @@
+public class fibonacci {
+    private static int fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        int last=fibo(n-1);
+        int slast=fibo(n-2);
+        return last+slast;
+    }
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(fibo(n));
+    }
+    
+}
